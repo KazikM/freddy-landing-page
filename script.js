249667d0944ee6b147cecbbd1d4389a818883d1e
Katarzyna Marcinkiewicz <kazik.em@gmail.com>
2017-09-07 11:52:42 +0200

    Make the header

---
+++ script.js
@@ -0,0 +1,21 @@
+const arrow = document.querySelector('.js-scroll')
+const content = document.querySelector('#content')
+
+arrow.addEventListener('click', () => {
+  content.scrollIntoView({ behavior: "smooth" })
+})
+
+const typeWriter = selector => {
+  const el = document.querySelector(selector)
+  const text = el.innerText
+
+    ; (function _type(i = 0) {
+      if (i === text.length) return
+
+      el.innerHTML =
+        text.substring(0, i + 1) + '<span aria-hidden="true"></span>'
+      setTimeout(() => _type(i + 1), 100)
+    })()
+}
+
+// typeWriter(".js-type-writer")
