a30a1e2319c5a3910bb47b7ebef837d833c7d78b
Katarzyna Marcinkiewicz <kazik.em@gmail.com>
2017-09-09 21:13:54 +0200

    Fix bugs

--- js/script.js
+++ js/script.js
@@ -62,3 +62,13 @@ const typeWriter = selector => {
 }
 
 // typeWriter(".js-type-writer")
+
+var textarea = document.querySelector('textarea');
+
+textarea.addEventListener('keydown', function () {
+  var el = this;
+  setTimeout(function () {
+    el.style.cssText = 'height:auto; padding:0';
+    el.style.cssText = 'height:' + el.scrollHeight + 'px';
+  }, 0);
+});
